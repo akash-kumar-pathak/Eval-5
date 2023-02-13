17d543e32e8ed4993381182d223e34e3103a3c70
akp07 <pathakakash07>
2023-02-13 17:48:28 +0530

    changes

---
+++ pages/contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const contact = () => {
+  return (
+    <div>
+      <h2>Education</h2>
+      <p>Completed 12th in Science from Navy Children School, Mumbai</p>
+      <h2>Work Experience</h2>
+      <p>
+        Currently working as an Associate Software Developer at Aster DM
+        Healthcare
+      </p>
+    </div>
+  );
+};
+
+export default contact;
